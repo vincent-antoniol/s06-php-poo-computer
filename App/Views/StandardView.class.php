dcf9339e26fdf70c0c3f522b1daeb252d765ac91
Romain Tello <romain.tello@gmail.com>
2020-10-21 13:30:43 +0200

    Add StandardView and HomeView classes

---
+++ App/Views/StandardView.class.php
@@ -0,0 +1,37 @@
+<?php
+
+class StandardView
+{
+    private $templates;
+
+    /**
+     * Create a new StandardView object
+     * 
+     * @param array $templates List of templates to be displayed inside the view
+     * @param array $variables (Optional) Associative array matching each required variable name with its value
+     */
+    public function __construct(array $templates, array $variables = [])
+    {
+        $this->templates = $templates;
+        $this->variables = $variables;
+    }
+
+    /**
+     * Render the view as HTML page
+     */
+    public function render()
+    {
+        // Crée une variable pour chaque couple de nom de variable/valeur reçu en entrée
+        foreach ($this->variables as $varName => $value) {
+            $$varName = $value;
+        }
+
+        include './templates/head.tpl.php';
+        // Charge tous les templates dans l'ordre
+        foreach ($this->templates as $template) {
+            include './templates/' . $template . '.tpl.php';
+        }
+        include './templates/footer.tpl.php';
+        include './templates/foot.tpl.php';
+    }
+}
